878c42d3fa7524fc8f57c6e5d9a23b5dd82c541d
Student <example@example.com>
2019-10-28 15:53:28 -0700

    changes to JS variable naming for clarity

--- js/scripts.js
+++ js/scripts.js
@@ -3,12 +3,12 @@ $(document).ready(function() {
   $("#blanks form").submit(function(event) {
     event.preventDefault();
 
-    var gList = [1,2,3,4,5,6].map(function(index) {
-      return $("input#item" + index).val();
+    var items = [1,2,3,4,5,6].map(function(item) {
+      return $("input#item" + item).val();
     });
 
-    gList.sort().forEach(function(list,index) {
-      $("#list" + (index + 1)).text(list.toUpperCase());
+    items.sort().forEach(function(item, index) {
+      $("#list" + (index + 1)).text(item.toUpperCase());
     });
 
 
